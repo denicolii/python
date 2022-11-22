3ef9057541833caccf39681b1c1b39e7b6184c9b
denicolii <110945232+denicolii@users.noreply.github.com>
2022-11-22 17:22:18 -0300

    Add files via upload
    Algoritmo que leia o tamanho do lado de um quadrado e informe a área e o perímetro do quadrado.

---
+++ area.py
@@ -0,0 +1,6 @@
+lado = float(input('Digite o tamanho do lado do quadrado: '))
+area = lado*lado
+perimetro = lado*4
+
+print ('A área tem o valor de: ',area)
+print('o pe´rimetro tem o valor de: ', perimetro)
