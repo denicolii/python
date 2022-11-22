52d2de0a677ac187719654f36128df496194e4e2
denicolii <110945232+denicolii@users.noreply.github.com>
2022-11-22 17:06:35 -0300

    Add files via upload
    Algoritmo que lê  as 3 notas obtidas por um aluno e calcula a média das notas.

---
+++ notas.py
@@ -0,0 +1,6 @@
+n1 = float(input('Digite sua primeira nota: '))
+n2 = float(input('Digite sua segunda nota: '))
+n3 = float(input('Digite sua terceira nota: '))
+media = (n1 + n2 + n3) / 3
+
+print ('A média de suas notas é igual a; ', media)
