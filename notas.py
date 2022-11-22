f6ab1ea136c68cc968448e09a1e2c184fb37c3f5
denicolii <110945232+denicolii@users.noreply.github.com>
2022-11-22 17:16:21 -0300

    Update notas.py

--- notas.py
+++ notas.py
@@ -2,5 +2,6 @@ n1 = float(input('Digite sua primeira nota: '))
 n2 = float(input('Digite sua segunda nota: '))
 n3 = float(input('Digite sua terceira nota: '))
 media = (n1 + n2 + n3) / 3
+media = round((media),2)
 
 print ('A média de suas notas é igual a; ', media)
