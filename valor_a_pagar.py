03806b318d915ae3e5fda2be422311dffa6fff48
denicolii <110945232+denicolii@users.noreply.github.com>
2022-11-22 17:02:06 -0300

    Add files via upload
    algoritmo que lê o valor unitário e a quantidade de lanches pedidos e, calcule o valor total a  ser pago pelo pedido.

---
+++ valor_a_pagar.py
@@ -0,0 +1,5 @@
+unitario = float(input('Digite o valor unitário do produto: '))
+quantidade = float(input('Quantos deste produto você deseja comprar? '))
+total = unitario*quantidade
+
+print ('O valor total a pagar por esses produtos é de: ', total, 'reais')
