82d2e49611980b7daf47ffcfa91018c4d63da9b5
denicolii <110945232+denicolii@users.noreply.github.com>
2022-11-22 17:26:24 -0300

    Add files via upload

---
+++ Fahrenheit.py
@@ -0,0 +1,5 @@
+tempF = float(input('Digite a temperatura em Fahrenheit: '))
+transf = (tempF - 32) / 1.8
+transf = round((transf),1)
+
+print('A transformação de fahrenheit para celsius é igual a: ', transf)
