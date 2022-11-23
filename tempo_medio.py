9b9c61ab3f9d8ff39366a80cb77ca1ae374fbf63
denicolii <110945232+denicolii@users.noreply.github.com>
2022-11-22 21:09:41 -0300

    Add files via upload
    Algoritmo que após a entrada de uma determinada distância entre dois pontos(Km), e uma determinada velocidade(Km/h), diga qual o tempo médio que levará para chegada à esse local.

---
+++ tempo_medio.py
@@ -0,0 +1,5 @@
+distancia  = int(input('Qual a distancia de um ponto a outro? '))
+velocidade = int(input('Qual a velocidade (km/h) de seu veículo?'))
+media = distancia/velocidade
+
+print ('O tempo médio que você irá gastar é de: ', media , 'horas'  )
