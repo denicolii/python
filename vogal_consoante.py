37cb501547885d34d995ec83e0906e3c36b069d5
Ana Denicoli <110945232+denicolii@users.noreply.github.com>
2025-03-19 16:20:51 -0300

    Add files via upload
    Código que retorna se a letra digitada é uma vogal ou consoante.

---
+++ vogal_consoante.py
@@ -0,0 +1,7 @@
+# código que retorna se uma letra é vogal ou uma consoante.
+letra = input('Digite uma letra:')
+if letra == ('a'or 'e'or 'i'or 'o'or 'u') or ('A'or 'E'or 'I'or 'O'or 'U'):
+    print('Vogal!')
+else :
+    print('Consoante')
+     
